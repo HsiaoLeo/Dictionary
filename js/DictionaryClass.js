21c2813c98181c630502da91e04c2b9b55c03b1a
Leo Hsiao (蕭君翰) <Leo.Hsiao@quantatw.com>
2022-05-16 09:32:44 +0800

    add word counter

--- js/DictionaryClass.js
+++ js/DictionaryClass.js
@@ -5,6 +5,13 @@ class Words{
     sentance_cn
     similar_word //array
     part_of_speech
+	static #wordCount = 0
+	static get wordCount(){
+		return Words.#wordCount.valueOf();
+	}
+	static countDecrease(){
+		Words.#wordCount.decrease();
+	}
     constructor({w,wc,s,sc,sw,pos}){
         this.word=w.toLowerCase();
         this.word_cn=wc;
@@ -12,6 +19,10 @@ class Words{
         this.sentance_cn=sc;
         this.similar_word=sw;
         this.part_of_speech=pos;
+		if(!Words.#wordCount){
+			Words.#wordCount = new Counter()
+		}
+		Words.#wordCount.increment();
         return Object.seal(this);
     }
 }
@@ -75,6 +86,7 @@ class WordNode{
     remove(){
         this.data=undefined;
         this.isText=false;
+		Words.countDecrease();
     }
     nextNode(c){
         return this.next[c.toUpperCase()];
@@ -92,6 +104,10 @@ class Counter{
         this.#val ++;
         return this;
     }
+	decrease(){
+		this.#val --;
+        return this;
+	}
     valueOf(){
         return this.#val;
     }
